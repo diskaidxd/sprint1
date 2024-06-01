7c7f9f5a008eb5e989b855321992688310098c08
John A <32318182+diskaidxd@users.noreply.github.com>
2024-05-31 19:33:19 -0500

    Update top_10_revenue_categories.sql

--- assignment/queries/top_10_revenue_categories.sql
+++ assignment/queries/top_10_revenue_categories.sql
@@ -5,3 +5,22 @@
 -- last one will be Revenue, with the total revenue of each catgory.
 -- HINT: All orders should have a delivered status and the Category and actual 
 -- delivery date should be not null.
+SELECT pcnt.product_category_name_english as Category,
+COUNT(DISTINCT oop.order_id) AS Num_order,
+SUM(oop.payment_value) as Revenue
+FROM olist_orders oo 
+JOIN olist_order_items ooi 
+ON oo.order_id  = ooi.order_id 
+JOIN olist_products op 
+ON ooi.product_id = op.product_id 
+JOIN product_category_name_translation pcnt 
+ON op.product_category_name = pcnt.product_category_name 
+JOIN olist_order_payments oop 
+ON oop.order_id = oo.order_id 
+WHERE ooi.order_id = oo.order_id and 
+oo.order_status = 'delivered' and 
+oo.order_delivered_customer_date is not null and
+op.product_category_name is not null
+GROUP BY Category
+ORDER BY Revenue DESC
+LIMIT 10
